4ef1dc4c74fb1dfd25063b3c52ef2544f1d16ee0
dev <rogerio.pereira@ifto.edu.br>
2025-04-22 20:59:52 -0300

    versão 1.02

--- chamadaApp/admin.py
+++ chamadaApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from chamadaApp.models import Cliente, Categoria, Telefone, Plano
 
 # Register your models here.
+admin.site.register(Cliente)
+admin.site.register(Categoria)
+admin.site.register(Telefone)
+admin.site.register(Plano)
--- chamadaApp/models.py
+++ chamadaApp/models.py
@@ -7,15 +7,24 @@ class Telefone(models.Model):
     numero_telefone=models.CharField(max_length=200, null=False)
     tipo_telefone= models.CharField(max_length=200, null=False)
 
+    def __str__(self):
+        return self.numero_telefone
+
 class Categoria(models.Model):
     id_categoria = models.AutoField(primary_key=True)
     nome_categoria =models.CharField(max_length=200, null=False)
+
+    def __str__(self):
+        return self.nome_plano
+
 
 
 class Plano(models.Model):
     id_plano= models.AutoField(primary_key=True)
     nome_plano =models.CharField(max_length=200, null=False)
     valor_plano =models.CharField(max_length=200, null=False)
+    def __str__(self):
+        return self.nome_plano
 
 
 class Cliente(models.Model):
@@ -27,4 +36,7 @@ class Cliente(models.Model):
     id_telefone =models.ForeignKey(Telefone, models.DO_NOTHING, db_column='id_telefone', blank=True, null=False)
     id_categoria =models.ForeignKey(Categoria,models.DO_NOTHING,db_column='id_categoria', blank=True, null=False)
     id_plano = models.ForeignKey(Plano, models.DO_NOTHING, db_column='id_plano', blank=True, null=True)
+
+    def __str__(self):
+        return self.nome_plano
 
